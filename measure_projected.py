521f7a6641e92bf04e8691d82e6b1304252e5c58
Vikenes <vetle.vkns@gmail.com>
2023-11-21 07:42:38 +0100

    Show fig instead of saving. Added comments at the beginning

--- measure_projected.py
+++ measure_projected.py
@@ -5,7 +5,7 @@ from scipy.interpolate import InterpolatedUnivariateSpline as ius
 from scipy.integrate import simps
 
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 matplotlib.rcParams['text.usetex'] = True
@@ -15,6 +15,21 @@ matplotlib.rcParams['text.latex.preamble'] = r'\usepackage{physics}'
 params = {'xtick.top': True, 'ytick.right': True, 'xtick.direction': 'in', 'ytick.direction': 'in'}
 plt.rcParams.update(params)
 
+"""
+Use mamba envir "halomodel" to run this script, as the Corrfunc package 
+is installed there. 
+In order for the package 'physics' to work in the matplotlib.rcParams,
+load the tex module: "module load texlive". 
+
+Computes proj. corrfunc, w_p(r_perp) with data from Ruan. 
+It's computed in two ways to compare solutions. 
+1) Integrating xi(r_para, r_perp) over r_para using galaxy position data
+2) Integrating xi(r) data over r, using that r^2=r_perp^2 + r_para^2
+
+Resulting plot shows that the two methods approx. yield the same result  
+
+
+"""
 box_size = 2000.0
 pos1 = np.load(f'/mn/stornext/d8/data/chengzor/void_abacussummit/data/multi/HOD_LOWZ_gal_pos_cos0_ph0_z0.25.npy')
 pimax = 100.0
@@ -83,8 +98,10 @@ ax0.plot(
 ax0.legend()
 ax0.set_xlabel(r'$r_{\perp} / (h^{-1}\mathrm{Mpc})$')
 ax0.set_ylabel(r'$r_{\perp} w_{p}(r_{\perp})$')
-plt.savefig(
-    "projected.pdf",
-    bbox_inches="tight",
-    pad_inches=0.05,
-)
+
+plt.show()
+#plt.savefig(
+#    "projected.pdf",
+#    bbox_inches="tight",
+#    pad_inches=0.05,
+#)
